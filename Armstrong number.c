e54181435891c6f17d4193cb6c3089ccc54cb875
nikhil-hehe <babitajha322@gmail.com>
2025-01-22 14:38:28 +0530

    Update Armstrong number.c

--- Armstrong number.c
+++ Armstrong number.c
@@ -28,3 +28,34 @@ int main(){
 	}
 	return 0;
 }
+
+
+//Function
+#include <stdio.h>
+#include <math.h>
+int armstrong(int);
+int armstrong(int num){
+	int a,i,rem,count,sum;
+	count=0;
+	a=num;
+	
+	while (a!=0){
+		a=a/10;
+		count++;
+	}
+	a=num;
+	while(a!=0){
+		rem=a%10;
+		sum=sum+pow(rem,count);
+		a=a/10;
+	}
+	
+	if (sum==num){
+		printf("armstrong no.");
+	}
+	
+	else{
+		printf("not armstrong no.");
+	}
+	return 0;
+}
