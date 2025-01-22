f6d44ec4c21e44d2877effb49c7c54d22c2cb6c5
nikhil-hehe <babitajha322@gmail.com>
2025-01-22 11:29:23 +0530

    Update Pattern-printing.c

--- Pattern-printing.c
+++ Pattern-printing.c
@@ -1,5 +1,10 @@
-//AB 
-/*#include <stdio.h>
+/*
+A 
+B A 
+A B A 
+B A B A 
+A B A B A*/
+#include <stdio.h>
 
 int main()
 {	int i,j;
@@ -17,9 +22,17 @@ int main()
 		printf("\n");
 	}
 	return 0;
-}*/
+}
 
-/*#include <stdio.h>
+
+/*
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+*/
+#include <stdio.h>
 
 int main()
 {   int i,j;
@@ -31,10 +44,18 @@ int main()
         printf("\n");
     }
     return 0;
-}*/
+}
 
 
-//Pyramid pattern
+
+/*
+        *         
+      * * *       
+    * * * * *     
+  * * * * * * *   
+* * * * * * * * * 
+*/
+	
 #include <stdio.h>
 
 int main()
@@ -52,5 +73,48 @@ int main()
 
 		printf("\n");
 	}
+	return 0;
+}
+
+
+//simple choice to operations
+#include <stdio.h>
+#include <math.h>
+
+int main()
+
+{
+	int choice;
+	float r,l,w,a,b,c,a_c,a_t,a_r,s;
+	printf("Enter choice\n1.Area of Circle\n2.Area of Triangle\n3.Area of Rectangle\n");
+	scanf("%d",&choice);
+
+	switch (choice) {
+	case 1:
+		printf("Enter the radius\n");
+		scanf("%f",&r);
+		printf("Area of circle is %f",2*3.1417*r);
+		break;
+	case 2:
+		printf("Enter the sides\n");
+		scanf("%f%f%f",&a,&b,&c);
+		s=(a+b+c)/2;
+		a_t=sqrt(s*(s-a)*(s-b)*(s-c));
+		printf("Area of triangle is %f",a_t);
+		break;
+	case 3:
+		printf("Enter the length and width\n");
+		scanf("%f%f",&l,&w);
+		a_r=l*w;
+		printf("Area of reactangle is %f",a_r);
+		break;
+
+	default:
+		printf("Enter a valid choice");
+		break;
+
+	}
+
+
 	return 0;
 }
