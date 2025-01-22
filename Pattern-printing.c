8df4efb1673bc8d83c938bd1919b1cde383e843a
nikhil-hehe <babitajha322@gmail.com>
2025-01-22 10:33:55 +0530

    Update Pattern-printing.c

--- Pattern-printing.c
+++ Pattern-printing.c
@@ -32,3 +32,25 @@ int main()
     }
     return 0;
 }*/
+
+
+//Pyramid pattern
+#include <stdio.h>
+
+int main()
+{	int i,j;
+	for (i=1; i<=5; i++) {
+		for (j=1; j<=(5*2)-1; j++) {
+
+			if (j>=5-(i-1) && (j<=5+(i-1))) {
+				printf("* ");
+			}
+			else {
+				printf("  ");
+			}
+		}
+
+		printf("\n");
+	}
+	return 0;
+}
