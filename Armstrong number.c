9b8a1e489398f3a892d164e46f5feb1b05fd5157
nikhil-hehe <babitajha322@gmail.com>
2025-01-22 14:29:17 +0530

    Create Armstrong number.c

---
+++ Armstrong number.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int a,i,num,rem,count,sum;
+	printf("enter the number\n");
+	scanf("%d",&num);
+	count=0;
+	a=num;
+	
+	while (a!=0){
+		a=a/10;
+		count++;
+	}
+	a=num;
+	while(a!=0){
+		rem=a%10;
+		sum=sum+pow(rem,count);
+		a=a/10;
+	}
+	
+	if (sum==num){
+		printf("armstrong no.");
+	}
+	
+	else{
+		printf("not armstrong no.");
+	}
+	return 0;
+}
