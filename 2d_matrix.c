7d82c9e0c8b9e700ca6cef3a0c2d36e72dceb194
nikhil-hehe <babitajha322@gmail.com>
2025-01-25 11:05:16 +0530

    Update 2d_matrix.c

--- 2d_matrix.c
+++ 2d_matrix.c
@@ -2,15 +2,29 @@
 #define MAX 100
 
 int main(){
-	int rows,columns,i,j,a[MAX][MAX];
+	int rows,columns,i,j,a[MAX][MAX],b[MAX][MAX];
 	printf("enter number of rows and columns\n");
 	scanf("%d%d",&rows,&columns);
+		printf("\nRow Major order\n");
 	for (i=0;i<rows;i++){
 		for (j=0;j<columns;j++){
 			scanf("%d",&a[i][j]);
 		}
 	}
 	
+	for (i=0;i<rows;i++){
+		for (j=0;j<columns;j++){
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\nColumn Major order\n");
+	
+	for (j=0;j<columns;j++){
+		for (i=0;i<rows;i++){
+			scanf("%d",&a[i][j]);
+		}
+	}
 	for (i=0;i<rows;i++){
 		for (j=0;j<columns;j++){
 			printf("%d ",a[i][j]);
