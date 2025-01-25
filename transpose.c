cdfdd0a1a304cb9178e3e8f6e5152e607f9b039b
nikhil-hehe <babitajha322@gmail.com>
2025-01-25 11:31:14 +0530

    Update transpose.c

--- transpose.c
+++ transpose.c
@@ -2,19 +2,22 @@
 #define MAX 100
 
 int main(){
-	int rows,columns,i,j,a[MAX][MAX],sum=0;
+	int rows,columns,i,j,a[MAX][MAX],b[MAX][MAX],sum=0;
 	printf("enter number of rows and columns\n");
 	scanf("%d%d",&rows,&columns);
 		printf("\nRow Major order\n");
 	for (i=0;i<rows;i++){
 		for (j=0;j<columns;j++){
 			scanf("%d",&a[i][j]);
+			b[j][i]=a[i][j];
 		}
 	}
+
+
 	printf("\nTranspose\n");
 	for (i=0;i<rows;i++){
 		for (j=0;j<columns;j++){
-			printf("%d ",a[j][i]);
+			printf("%d ",b[i][j]);
 		}
 		printf("\n");
 	}
