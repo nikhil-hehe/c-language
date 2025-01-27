b3fe310a1f8944ec7094a290dc2333fbea4b993b
Nikhil Jha <babitajha322@gmail.com>
2025-01-27 10:29:00 +0530

    Update z1_input_string_doubt.c

--- z1_input_string_doubt.c
+++ z1_input_string_doubt.c
@@ -17,3 +17,27 @@ int main(){
 	n=input(str,100);
 	printf("%s",str);
 }
+
+
+// corrected code by gpt
+
+#include <stdio.h>
+
+int input(char str[], int n) {
+    int i = 0, ch;
+    while ((ch = getchar()) != '\n' && i < n - 1) {  // Corrected: proper assignment and condition
+	    //here is the mistake
+        str[i] = ch;
+        i++;
+    }
+    str[i] = '\0';  // Null-terminate the string
+    return i;  
+}
+
+int main() {
+    char str[100];
+    int n;
+    n = input(str, 100);
+    printf("%s", str);  // Print the entered string
+}
+
